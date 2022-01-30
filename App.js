a18578fabd7ce5f5891dbddfa94c8994ebd893e9
dkutelov <dkutelov@digitals.bg>
2022-01-30 17:38:12 +0200

    Home screen layout

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+
+const isAndorid = Platform.OS === "android";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeAreaView
+        style={{ ...styles.container, marginTop: isAndorid ? 20 : 0 }}
+      >
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>Search</Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  search: {
+    backgroundColor: "blue",
+    padding: 15
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "green",
+    padding: 15
+  }
 });
