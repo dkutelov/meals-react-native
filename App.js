99f2510c273d0faa63350c3eec41c25ddf9f10f2
dkutelov <dkutelov@digitals.bg>
2022-02-18 09:46:36 +0200

    Scafolding restaurant screen and info

--- App.js
+++ App.js
@@ -1,44 +1,14 @@
 import React from "react";
-import { Searchbar } from "react-native-paper";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant.screen";
 //const isAndorid = Platform.OS === "android";
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = React.useState("");
-
-  const onChangeSearch = (query) => setSearchQuery(query);
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-          />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 15,
-  },
-  list: {
-    flex: 1,
-    backgroundColor: "green",
-    padding: 15,
-  },
-});
---
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StyleSheet, View, SafeAreaView, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
+
+import { RestaurantInfo } from "../components/restautant-info.component";
+
+export const RestaurantsScreen = () => {
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+      </View>
+      <View style={styles.list}>
+        <RestaurantInfo />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 15,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "green",
+    padding: 15,
+  },
+});
