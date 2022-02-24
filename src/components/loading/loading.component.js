e9e36b3e1f44174ffa2c7580da30428fe4343123
dkutelov <dkutelov@digitals.bg>
2022-02-24 16:59:03 +0200

    Activity indicator margin refactoring

--- src/components/loading/loading.component.js
+++ src/components/loading/loading.component.js
@@ -3,7 +3,10 @@ import { colors } from "../../infrastructure/theme/colors";
 
 import { LoadingContainer, Loading } from "./loading.styles";
 
-export const LoadingIndicator = ({ size = 50, color = colors.ui.primary }) => (
+export const LoadingIndicator = ({
+  size = 50,
+  color = colors.ui.secondary,
+}) => (
   <LoadingContainer>
     <Loading size={size} animating={true} color={color} />
   </LoadingContainer>
--- src/components/loading/loading.styles.js
+++ src/components/loading/loading.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components/native";
 import { ActivityIndicator } from "react-native-paper";
 
 export const Loading = styled(ActivityIndicator)`
-  margin-left: -25px;
+  margin-left: -${(props) => props.size / 2}px;
 `;
 
 export const LoadingContainer = styled.View`
