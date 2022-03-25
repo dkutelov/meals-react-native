017a04646dc0030aba8e96573ab30eff4c7bd0a3
dkutelov <dkutelov@digitals.bg>
2022-03-25 17:57:36 +0200

    Login and register buttons

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components/native";
-import { ImageBackground } from "react-native";
+import { colors } from "../../../infrastructure/theme/colors";
+import { Button } from "react-native-paper";
 
-export const AccountBackground = styled(ImageBackground).attrs({
+export const AccountBackground = styled.ImageBackground.attrs({
   source: require("../../../../assets/home_bg.jpg"),
 })`
   flex: 1;
@@ -14,4 +15,16 @@ export const AccountCover = styled.View`
   width: 100%;
   height: 100%;
   background-color: rgba(255, 255, 255, 0.3);
+`;
+
+export const AccountContainer = styled.View`
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: ${({ theme }) => theme.space[4]};
+  margin-top: ${({ theme }) => theme.space[2]};
+`;
+
+export const AuthButton = styled(Button).attrs({
+  color: colors.brand.primary,
+})`
+  padding: ${(props) => props.theme.space[2]};
 `;
