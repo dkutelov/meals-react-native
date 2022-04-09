0cda951bdfaa51ddfd09f2626ba8ae1eb53bbe2f
dkutelov <dkutelov@digitals.bg>
2022-04-09 16:37:26 +0300

    Account screen lottie animation

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@infrastructure": "./src/infrastructure",
+            "@infrastructure/*": "./src/infrastructure/*",
+            "@services": "./src/services",
+            "@services/*": "./src/services/*",
+            "@components": "./src/components",
+            "@components/*": "./src/components/*",
+            "@features": "./src/features",
+            "@features/*": "./src/features/*",
+          },
+        },
+      ],
+    ],
   };
 };
