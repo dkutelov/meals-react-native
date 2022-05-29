afda343cb28b22154f1f9d4dc602d048b9945b5f
dkutelov <dkutelov@digitals.bg>
2022-05-29 16:14:47 +0300

    Google geocode api

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,12 +1,26 @@
 const { locations: locationsMock } = require("./geocode.mock");
+const functions = require("firebase-functions");
 const url = require("url");
 
-module.exports.geocodeRequest = (req, res) => {
+module.exports.geocodeRequest = (req, res, client) => {
   try {
-    const { city } = url.parse(req.url, true).query;
-    const locationMock = locationsMock[city.toLowerCase()];
+    const { city, mock } = url.parse(req.url, true).query;
+    if (mock === "true") {
+      const locationMock = locationsMock[city.toLowerCase()];
 
-    res.json(locationMock);
+      return res.json(locationMock);
+    }
+
+    client
+      .geocode({
+        params: {
+          address: city,
+          key: functions.config().google.key,
+        },
+        timeout: 1000,
+      })
+      .then((response) => res.json(response.data))
+      .catch((e) => res.status(400).send(e.res.data.error_message));
   } catch (error) {
     console.log(error);
     res.status(500).send("Error getting location");
