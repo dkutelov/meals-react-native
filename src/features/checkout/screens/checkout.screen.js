98cdae34b3c46c5c50f5961fb1d867ac266b7904
dkutelov <dkutelov@digitals.bg>
2022-06-05 11:23:14 +0300

    checkout screen

--- src/features/checkout/screens/checkout.screen.js
+++ src/features/checkout/screens/checkout.screen.js
@@ -1,12 +1,24 @@
-import React, { useContext } from "react";
-import { CreditCardInput } from "../components/credit-card.component";
+import React, { useContext, useState } from "react";
+import { ScrollView } from "react-native";
+import { List } from "react-native-paper";
+
 import { SafeArea } from "@components/utilities/safe-area.component";
 import { Text } from "@components/typography/text.component";
 import { Spacer } from "@components/spacer/spacer.component";
 import { CartContext } from "@services/cart/cart.context";
-import { CartIconContainer, CartIcon } from "../components/checkout.styles";
+import { CreditCardInput } from "../components/credit-card.component";
+import { RestaurantInfoCard } from "@features/restaurants/components/restautant-info-card.component";
+import {
+  CartIconContainer,
+  CartIcon,
+  NameInput,
+  PayButton,
+  ClearButton,
+} from "../components/checkout.styles";
+
 export const CheckoutScreen = () => {
-  const { cart, restaurant } = useContext(CartContext);
+  const { cart, restaurant, sum, clearCart } = useContext(CartContext);
+  const [name, setName] = useState("");
 
   if (!cart || !restaurant) {
     return (
@@ -20,9 +32,41 @@ export const CheckoutScreen = () => {
   }
   return (
     <SafeArea>
-      <Text>{JSON.stringify(cart)}</Text>
-      <Text>{JSON.stringify(restaurant)}</Text>
-      <CreditCardInput />
+      <RestaurantInfoCard restaurant={restaurant} />
+      <ScrollView>
+        <Spacer position="left" size="large">
+          <Spacer position="top" size="large">
+            <Text>Your Order</Text>
+          </Spacer>
+          <List.Section>
+            {cart.map(({ item, price }, index) => (
+              <List.Item
+                key={`${item}-${index}`}
+                title={`${item} - ${price / 100}`}
+              />
+            ))}
+          </List.Section>
+          <Text>Total: {sum / 100}</Text>
+        </Spacer>
+        <NameInput
+          label="Name"
+          value={name}
+          onChangeText={(t) => {
+            setName(t);
+          }}
+        />
+        <Spacer position="top" size="large">
+          {name.length > 0 && <CreditCardInput name={name} />}
+        </Spacer>
+        <Spacer position="top" size="xxl" />
+        <PayButton icon="cash-usd" mode="contained" onPress={() => {}}>
+          Pay
+        </PayButton>
+        <Spacer position="top" size="large" />
+        <ClearButton icon="cart-off" mode="contained" onPress={clearCart}>
+          Clear Cart
+        </ClearButton>
+      </ScrollView>
     </SafeArea>
   );
 };
